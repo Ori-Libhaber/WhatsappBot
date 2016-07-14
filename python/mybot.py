a83ea9ec02a11787e473282b10863568388918c7
Yoav Salarios <yoavs@redbend.com>
2016-07-14 05:18:47 +0300

    fix

--- python/mybot.py
+++ python/mybot.py
@@ -42,57 +42,52 @@ def sendMessage(message):
 	msgbox.send_keys(message)
 	driver.find_element_by_class_name("icon-send").click()
 
-stam = driver.find_element_by_class_name("message-list").find_elements_by_class_name("message-text")
-txt = driver.find_element_by_class_name("message-list").get_attribute("innerHTML")
+def parser():
+	txt = driver.find_element_by_class_name("message-list").get_attribute("innerHTML")
+	txt_find= "inverse-text-direction selectable-text"+'"'+ " dir=" +'"'+"rtl"+'"'+">"
+	txt_other_find = "class="+'"'+"emojitext selectable-text"+'"'+" dir="+'"'+"ltr"+'"'+">"
+	authour_txt = "<span class="+'"'+"emojitext"+'"'+">"
+	prev_auth = ""
 
+	for elem in txt.split("bubble bubble"):
+		authour = ""
+		txt_find_len = 50
+		strt = elem.find(txt_find)
+		if (strt<0):
+			strt = elem.find(txt_other_find)
+			if (strt<0):
+				continue
+			txt_find_len = 44
+		new_txt = elem[strt+txt_find_len:]
+		if (new_txt.find("href")>=0):
+			continue
+		end = new_txt.find("<")
+		if (elem.find(authour_txt)>=0):
+			tmp_authour = elem[elem.find(authour_txt)+24:]
+			end_tmp = tmp_authour.find("<")
+			authour = tmp_authour[:end_tmp]
+			prev_auth = authour
+		print elem[elem.find("[")+1:elem.find("]")]
+		print new_txt[0:end]
+		if (authour==""):
+			if (elem.find("+972 54-772-0957")>=0):
+				authour = "צחי לפידות"
+			elif (elem.find("+972 52-616-3001")>=0):
+				authour = "יואב"
+			else:
+				authour = prev_auth
+		print authour
+		arr = dico.get(authour,[authour])
+	   arr.append(new_txt[0:end])
+	   dico[authour]=arr
+	   print "========================================"
+	  ##  if dico.__contains__(authour):
 
-txt_find= "inverse-text-direction selectable-text"+'"'+ " dir=" +'"'+"rtl"+'"'+">"
-txt_other_find = "class="+'"'+"emojitext selectable-text"+'"'+" dir="+'"'+"ltr"+'"'+">"
-authour_txt = "<span class="+'"'+"emojitext"+'"'+">"
-prev_auth = ""
-
-for elem in txt.split("bubble bubble"):
-    authour = ""
-    txt_find_len = 50
-    strt = elem.find(txt_find)
-    if (strt<0):
-        strt = elem.find(txt_other_find)
-        if (strt<0):
-            continue
-        txt_find_len = 44
-    new_txt = elem[strt+txt_find_len:]
-    if (new_txt.find("href")>=0):
-        continue
-    end = new_txt.find("<")
-    if (elem.find(authour_txt)>=0):
-        tmp_authour = elem[elem.find(authour_txt)+24:]
-        end_tmp = tmp_authour.find("<")
-        authour = tmp_authour[:end_tmp]
-        prev_auth = authour
-    print elem[elem.find("[")+1:elem.find("]")]
-    print new_txt[0:end]
-    if (authour==""):
-        if (elem.find("+972 54-772-0957")>=0):
-            authour = "צחי לפידות"
-        elif (elem.find("+972 52-616-3001")>=0):
-            authour = "יואב"
-        else:
-            authour = prev_auth
-    print authour
-    arr = dico.get(authour,[authour])
-    arr.append(new_txt[0:end])
-    dico[authour]=arr
-    print "========================================"
+		print "========================================"
+>>>>>>> fix
 
 
 time.sleep(2)
-# Scroll up to get more messages
-try:
-	icon_refresh = driver.find_element_by_class_name("icon-refresh")
-except:
-	pass
-else:
-	icon_refresh.click()
 
 def scorllUp():
 	# Scroll up to get more messages
@@ -103,23 +98,19 @@ def scorllUp():
 	else:
 		icon_refresh.click()
 
+
+scorllUp()
+
 # Main loop
 while True:
-	stam = driver.find_element_by_class_name("message-list").find_elements_by_class_name("message-text")
-	for pices in stam:
-		print pices.text
-		inr_txt = pices.get_attribute("innerHTML")
-		strt = inr_txt.find("[")
-		end = inr_txt.find("]")
-		print inr_txt[strt:end]
+	parser()
 
 	time.sleep(2)
 
-	scorllUp()
+	# Check last message
 
+	# Send relevant message
 	sendMessage("Hello World!!!")
-
-	msg_groups = driver.find_elements_by_class_name("msg");
 
 	time.sleep(5)
 	#driver.quit()
